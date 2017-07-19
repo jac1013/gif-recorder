f1cb1c4c2baba8d4d30209f90d62243446f79595
Joseph Arrieta <jac.wshmstr@gmail.com>
2017-07-18 20:08:19 -0500

    this is just code so that we can easily test that NodeJS is truly run it in the Electron App, this commit will be removed before merging

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,30 +1,21 @@
-<<<<<<< HEAD
-import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
-=======
 import { Component, OnInit } from '@angular/core';
->>>>>>> 014edbf32c9bad6c1c7a63dd7a1209e628f1e3a9
+import { CommandRunner } from '../../bizanz-record/command-runner.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-<<<<<<< HEAD
-export class HomeComponent implements OnInit, OnChanges {
-  ngOnChanges(changes: SimpleChanges): void {
-  }
-=======
 export class HomeComponent implements OnInit {
->>>>>>> 014edbf32c9bad6c1c7a63dd7a1209e628f1e3a9
   title = `App works !`;
 
-  constructor() { }
+  constructor(private commandRunner: CommandRunner) { }
 
   ngOnInit() {
   }
 
+  runCommand() {
+    this.commandRunner.run();
+  }
+
 }
-<<<<<<< HEAD
-//delete later.
-=======
->>>>>>> 014edbf32c9bad6c1c7a63dd7a1209e628f1e3a9
